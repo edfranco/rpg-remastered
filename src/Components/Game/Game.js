953ab8a9a8e9eb24f04d94f14b45912e7be4322e
edfranco <edfranco415@gmail.com>
2019-11-01 22:27:06 -0700

    created rolling function

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -5,20 +5,38 @@ import Profile from '../Profile/Profile';
 import './Game.css'
 
 const SKILLS = [
-    'Strength',
-    'Dexterity',
-    'Constitution',
-    'Intelligenece',
-    'Wisdom',
-    'Charisma'
+    'strength',
+    'dexterity',
+    'constitution',
+    'intelligence',
+    'wisdom',
+    'charisma'
 ]
 
 class Game extends Component {
     state = {
         skillIndex: 0,
         skillRank: 0,
+        name: 'Adventurer',
+        strength: 0,
+        dexterity: 0,
+        constitution: 0,
+        intelligence: 0,
+        wisdom: 0,
+        charisma: 0,
+        hp: 0,
+        dmg: 0,
         displayStat: false
-    }
+    };
+
+    setStat = () => {
+        const hasExceededArrLimit = this.state.skillIndex >= SKILLS.length - 1;
+        const stat = Math.floor(Math.random() * 20) + 1;
+        this.setState({
+            [SKILLS[this.state.skillIndex]]: stat,
+            skillIndex: hasExceededArrLimit ? 0 : this.state.skillIndex + 1
+        });
+    };
 
     render() {
         return (
@@ -27,10 +45,20 @@ class Game extends Component {
                     <h1>Roll the Dice For Stats</h1>
                     <h4>Roll for {SKILLS[this.state.skillIndex]}</h4>
                     {this.state.displayStat && <p>Your {SKILLS[this.state.skillIndex]} is now {this.state.skillRank}  </p>}
-                    <button>Roll</button>
+                    <button onClick={this.setStat}>Roll</button>
                 </div>
                 <div className="profile-container">
-                    <Profile />
+                    <Profile
+                        name={this.state.name}
+                        strength={this.state.strength}
+                        dexterity={this.state.dexterity}
+                        constitution={this.state.constitution}
+                        intelligence={this.state.intelligence}
+                        wisdom={this.state.wisdom}
+                        charisma={this.state.charisma}
+                        hp={this.state.hp}
+                        dmg={this.state.dmg}
+                    />
                 </div>
             </div>
         );
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -2,34 +2,35 @@ import React, { Component } from 'react';
 // internal styles
 import './Profile.css';
 
-class Profile extends Component {
-    state = {
-        health: 0,
-        strength: 0,
-        dexterity: 0,
-        constitution: 0,
-        intelligenece: 0,
-        wisdom: 0,
-        charisma: 0,
-    }
-
-    render() {
-        return (
-            <div className="profile">
-                <h1>You</h1>
-                <h4>HP:{this.state.health}</h4>
-                <img className="user-image" src="https://i.imgur.com/lixfsa3.jpg" />
-                <ul>
-                    <li>Strength: {this.state.strength} </li>
-                    <li>Dexterity: {this.state.dexterity}</li>
-                    <li>Constituion: {this.state.constitution}</li>
-                    <li>Intelligence: {this.state.intelligenece}</li>
-                    <li>Wisdom: {this.state.wisdom}</li>
-                    <li>Charisma {this.state.charisma}</li>
-                </ul>
+const Profile = ({ strength,
+    name,
+    dexterity,
+    constitution,
+    intelligence,
+    wisdom,
+    charisma,
+    hp,
+    dmg
+}) => {
+    return (
+        <div className="profile">
+            <h1>{name}</h1>
+            <div className="battle-stats">
+                <h4>HP: {hp} </h4>
+                <h4>DMG: {dmg}</h4>
             </div>
-        );
-    };
+
+            <img className="user-image" src="https://i.imgur.com/lixfsa3.jpg" />
+            <ul>
+                <li>Strength: {strength} </li>
+                <li>Dexterity: {dexterity}</li>
+                <li>Constituion: {constitution}</li>
+                <li>Intelligence: {intelligence}</li>
+                <li>Wisdom: {wisdom}</li>
+                <li>Charisma {charisma}</li>
+            </ul>
+        </div>
+    );
 };
 
 export default Profile;
