9fe9a858cb3d20d07e9cf297a7855e2590b2b6ba
edfranco <edfranco415@gmail.com>
2019-11-01 19:18:19 -0700

    basic folder strcuture and nav created

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// styles
+import './Nav.css';
+const Nav = () => {
+    return (
+        <nav className="nav">
+            <div className="header">
+                <h1>Not a Good RPG</h1>
+            </div>
+            <div className="personal-links">
+                <i class="fab fa-linkedin"></i>
+                <i class="fab fa-github-square"></i>
+                <i class="fab fa-github-square"></i>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
