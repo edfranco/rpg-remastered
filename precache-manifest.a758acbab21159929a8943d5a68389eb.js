981018663f56bcb28298688b937b20bc7d0ea130
edfranco <edfranco415@gmail.com>
2019-11-02 19:52:15 -0700

    Updates

---
+++ precache-manifest.a758acbab21159929a8943d5a68389eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7c592bf982601ea6ae255dab712fdf3",
+    "url": "/rpg-remastered/index.html"
+  },
+  {
+    "revision": "e8a130dfa2cc2da2cb8e",
+    "url": "/rpg-remastered/static/css/main.dbf70233.chunk.css"
+  },
+  {
+    "revision": "d35bf312d0b27f4f15c0",
+    "url": "/rpg-remastered/static/js/2.28a7d729.chunk.js"
+  },
+  {
+    "revision": "e8a130dfa2cc2da2cb8e",
+    "url": "/rpg-remastered/static/js/main.6f3a525e.chunk.js"
+  },
+  {
+    "revision": "8293e8ac6b89e6ae30f8",
+    "url": "/rpg-remastered/static/js/runtime-main.744a6dac.js"
+  }
+]);
